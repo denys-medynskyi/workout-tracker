dbdec30612ee5db746e6662673ebf490010870bf
Denys Medynskyi <denys.medynskyi@toptal.com>
2019-11-06 12:37:40 +0000

    Init repo

---
+++ src/components/WorkoutList/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './Header'
+
+const WorkoutList = () => {
+  return (
+    <div>
+      <Header text={WorkoutList} />
+    </div>
+  );
+}
+
+export default WorkoutList;
